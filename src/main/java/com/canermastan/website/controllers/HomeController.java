105fc4119b96811108d254000d77d37b51982c62
Caner Mastan <revilations@hotmail.com.tr>
2021-12-23 22:33:35 +0300

    endpoint degisti

--- src/main/java/com/canermastan/website/controllers/HomeController.java
+++ src/main/java/com/canermastan/website/controllers/HomeController.java
@@ -11,26 +11,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/showPdf")
+    @GetMapping("/pdf")
     public String showPdf(){
         return "pdf";
     }
 
-    @GetMapping("/pdf")
+    @GetMapping("/showpdf")
     public ResponseEntity<InputStreamResource> displayPdf() throws IOException {
         String fileName = "static/database.pdf";
-        File file = new ClassPathResource(fileName).getFile();
+        //File file = new ClassPathResource(fileName).getFile();
+        InputStream inputStream = new ClassPathResource(fileName).getInputStream();
         HttpHeaders headers = new HttpHeaders();
         headers.add("content-disposition", "inline;filename=" +fileName);
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(String.valueOf(inputStream)));
 
         return ResponseEntity.ok()
                 .headers(headers)
-                .contentLength(file.length())
                 .contentType(MediaType.parseMediaType("application/pdf"))
                 .body(resource);
     }
